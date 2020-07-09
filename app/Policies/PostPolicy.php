3e2cc6e6e52f8beaedf999c6edd45b05b72d30fc
YuukiOono1 <>
2020-07-09 16:43:48 +0900

    no message

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Policies;
+
+use App\Post;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    // index
+    // ?User -> nullable
+    public function viewAny(?User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    // show
+    public function view(?User $user, Post $post)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    // create
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    // update
+    public function update(User $user, Post $post)
+    {
+        return $user->id === $post->user_id
+        ? Response::allow()
+        : Response::deny('You do not own this post.');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    // delete
+    public function delete(User $user, Post $post)
+    {
+        return $user->id === $post->user_id
+        ? Response::allow()
+        : Response::deny('You do not own this post.');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function restore(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function forceDelete(User $user, Post $post)
+    {
+        //
+    }
+}
