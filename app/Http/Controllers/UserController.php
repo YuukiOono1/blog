93e92c57bcd7003d0e16b1b114f0eacba857675c
YuukiOono1 <>
2020-07-10 17:46:38 +0900

    [fix] posts pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -12,11 +13,12 @@ class UserController extends Controller
         // 記事に紐づいたユーザーの情報を取得
         $user = User::find($id);
 
-        // ユーザーに紐づいた記事を作成順に表示, ページネーションできない・・
-        $posts = $user->post
-            ->sortby('created_at');
-        //  ->paginate(1)
-        
+        // ユーザー紐づいた記事の一覧を作成順に表示
+        $posts = Post::with('user')->orderBy('created_at', 'desc')
+            ->whereHas('user', function ($query) use ($user) {
+                $query->where('name', $user->name);
+            })->paginate(1);
+
         return view('users.show', ['user' => $user, 'posts' => $posts]);
 
         // カテゴリーの詳細と同じで、 下記のようにならないの何故？
