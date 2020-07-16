0f786710d57ae82127a383429d38d74545279ef6
YuukiOono1 <>
2020-07-16 17:34:49 +0900

    [fix] User tabs

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,11 +21,11 @@
     <ul class="nav nav-tabs md-tabs nav-justified grey lighten-2" role="tablist">
         <li class="nav-item">
             <a class="nav-link active text-muted" data-toggle="tab" href="#panel555" role="tab">
-            <i class="fas fa-user-circle pr-2"></i>登録情報</a>
+            <i class="fas fa-user-circle pr-2"></i>記事一覧</a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-muted" data-toggle="tab" href="#panel666" role="tab">
-            <i class="fas fa-edit pr-2"></i>記事一覧</a>
+            <i class="fas fa-edit pr-2"></i>登録情報</a>
         </li>
     </ul>
     <!-- Nav tabs -->
@@ -37,63 +37,6 @@
 
     <!-- Panel 1 -->
     <div class="tab-pane fade in show active" id="panel555" role="tabpanel">
-        <!-- Card -->
-        <div class="card mt-4">
-            <div class="card-body">
-                <form method="GET" action="{{ route('users.edit', ['user' => $user]) }}">
-                    @csrf
-                    <div class="md-form mb-4">
-                        <label>名前</label>
-                        <input type="text" id="name" name="name" class="@error('name') is-valid @enderror form-control" required value="{{ $user->name }}">
-                        @error('name')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="md-form mb-4">
-                        <label>メールアドレス</label>
-                        <input type="email" id="email" name="email" class="@error('email') is-valid @enderror form-control" required value="{{ $user->email }}">
-                        @error('email')
-                            <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn default-color btn-block btn-rounded z-depth-1a"><span class="white-text">入力内容を確認する</span></button>
-                    <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-4">
-                        <a href="{{ route('password.request') }}">パスワードを変更される方はこちら</a>
-                    </p>
-                    <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2" data-toggle="modal" data-target="#modal-delete-{{ $user->id }}">
-                        <a class="text-danger">退会する方はこちら</a>
-                    </p>
-
-                    <!-- modal -->
-                    <div id="modal-delete-{{ $user->id }}" class="modal fade" tabindex="-1" role="dialog">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                                        <span aria-hidden="true">×</span>
-                                    </button>
-                                </div>
-                                <form method="POST" action="{{ route('users.destroy', ['user' => $user]) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <div class="modal-body">
-                                        ユーザーを退会しますか？（投稿した記事も削除されます）
-                                    </div>
-                                    <div class="modal-footer justify-content-between">
-                                        <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                                        <button type="submit" class="btn btn-danger">削除する</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- modal -->
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- Panel 2 -->
-    <div class="tab-pane fade" id="panel666" role="tabpanel">
 
         <div class="row row-cols-2 row-cols-md-3">
             @foreach ($posts as $post)
@@ -181,6 +124,64 @@
             @endforeach
         </div>
         {{ $posts->links() }}
+    </div>
+
+    <!-- Panel 2 -->
+    <div class="tab-pane fade" id="panel666" role="tabpanel">
+        <!-- Card -->
+        <div class="card mt-4">
+            <div class="card-body">
+                <form method="GET" action="{{ route('users.edit', ['user' => $user]) }}">
+                    @csrf
+                    <div class="md-form mb-4">
+                        <label>名前</label>
+                        <input type="text" id="name" name="name" class="@error('name') is-valid @enderror form-control" required value="{{ $user->name }}">
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="md-form mb-4">
+                        <label>メールアドレス</label>
+                        <input type="email" id="email" name="email" class="@error('email') is-valid @enderror form-control" required value="{{ $user->email }}">
+                        @error('email')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit" class="btn default-color btn-block btn-rounded z-depth-1a"><span class="white-text">入力内容を確認する</span></button>
+                    <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-4">
+                        <a href="{{ route('password.request') }}">パスワードを変更される方はこちら</a>
+                    </p>
+                    <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2" data-toggle="modal" data-target="#modal-delete-{{ $user->id }}">
+                        <a class="text-danger">退会する方はこちら</a>
+                    </p>
+
+                    <!-- modal -->
+                    <div id="modal-delete-{{ $user->id }}" class="modal fade" tabindex="-1" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <form method="POST" action="{{ route('users.destroy', ['user' => $user]) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div class="modal-body">
+                                        ユーザーを退会しますか？（投稿した記事も削除されます）
+                                    </div>
+                                    <div class="modal-footer justify-content-between">
+                                        <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                                        <button type="submit" class="btn btn-danger">削除する</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- modal -->
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
