cb90b6b0bcfa1f6c574e6f722f8faf56342b77ad
YuukiOono1 <>
2020-07-16 14:04:59 +0900

    [add] users update confirm

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,52 +1,31 @@
 @extends('layouts')
 
-@section('title', 'ユーザー名更新')
+@section('title', '確認画面')
 
 @section('content')
-<section class="form-elegant">
-
-    <!-- Grid row -->
-    <div class="row mt-4">
-
-        <!-- Grid column -->
-        <div class="mx-auto col-md-9 col-lg-7 col-xl-5">
-
-            <!--Form without header-->
-            <div class="card">
-
-                <div class="card-body">
-
-                    <!--Header-->
-                    <div class="text-center">
-                        <h3 class="dark-grey-text mb-5"><strong>ユーザーネームの変更</strong></h3>
-                        <p>変更前の名前 : {{ $user->name }}</h>
-                    </div>
-
-                    <form method="POST" action="{{ route('users.update', ['user' => $user->id]) }}">
-                        @method('PATCH')
-                        @csrf
-                        <div class="md-form">
-                            <input type="text" id="name" name="name" class="@error('name') is-valid @enderror form-control" required value="{{ old('name') }}">
-                            <label for="name">変更する名前</label>
-                            @error('name')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <button type="submit" class="btn default-color btn-block btn-rounded z-depth-1a"><span class="white-text">変更</span></button>
-                    </form>
-                    <!--Footer-->
-                    <div class="modal-footer mx-5 pt-3 mb-1">
-                        <a href="{{ route("users.show", ['user' => Auth::id()]) }}" class="blue-text ml-1">マイページへ戻る</a>
-                    </div>
-                </div>
-
-             </div>
-            <!--/Form without header-->
-
-        </div>
-        <!-- Grid column -->
-
+<div class="card mt-4">
+    <div class="card-body">
+        <form method="POST" action="{{ route('users.update', ['user' => $user]) }}">
+            @method('PATCH')
+            @csrf
+            <h1 class="dark-grey-text text-center mb-5"><strong>入力確認</strong></h1>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="row">ユーザーネーム</th>
+                        <td>{{ $data["name"] }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">メールアドレス</th>
+                        <td>{{ $data["email"] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button type="submit" class="btn default-color btn-block btn-rounded z-depth-1a"><span class="white-text">更新する</span></button>
+        </form>
+        <p class="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-4">
+            <a href="{{ route("users.show", ['user' => Auth::id()]) }}">マイページに戻る</a>
+        </p>
     </div>
-    <!-- Grid row -->
-</section>
+</div>
 @endsection
