65936ff0e52785eb8db16d8eeb4584a09a59135f
YuukiOono1 <>
2020-07-17 11:50:46 +0900

    [create] UserDeleteMail

---
+++ app/Mail/UserDeleteMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserDeleteMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from(config('mail.from.address'), config('mail.from.name')) // 送信元のメールアドレス
+            ->subject('退会手続き完了のお知らせ') // メールのタイトル
+            ->text('emails.user_delete') // テンプレート（平文）
+            ->with([
+                'register_url' => route('register'), 
+            ]);
+    }
+}
